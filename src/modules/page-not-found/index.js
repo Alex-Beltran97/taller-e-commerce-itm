2d523e4eb80c34369ea15bf27c626e1474365cbd
Pedro Beltran <pedroa.beltran.contractor@bbva.com>
2026-05-25 15:39:16 -0500

    Agregando pagina error 404

---
+++ src/modules/page-not-found/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+function renderPageNotFoundModule() {
+  return `
+    <section class="page-not-found">
+      <h1 class="page-not-found__item page-not-found__item--title">Page Not Found</h1>
+      <h2 class="page-not-found__item page-not-found__item--subtitle">Error 404</h2>
+    </section>
+  `;
+};
+
+function initPageNotFoundModule() {
+  const html = renderPageNotFoundModule();  
+  document.querySelector('#app').innerHTML = html;  
+};
+
+export default initPageNotFoundModule;
